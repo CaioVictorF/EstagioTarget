4529dca9789f604bab21a80318f9c0a0ddea30d8
Caio Victor <116463163+CaioVictorF@users.noreply.github.com>
2024-03-09 09:50:13 -0300

    first commit

---
+++ Questões Desafio/interruptores.c
@@ -0,0 +1,15 @@
+/*Ligo 2 interruptores, deixo ligado por alguns minutos e desligo apenas um.
+
+1° ida: Ao checar a primeira sala, se a lampada estiver ligada, ela é a lampada
+que está concetada ao interruptor q está ligado, se não, se estiver desligada 
+porém quente, ela é a lampada q está conectada ao interruptor q desligamo. Poderá
+ocorrer uma terceira condição, que é da lampada estar quente e fria, logo concluiremos
+neste caso, que ela é a lampada que está conectada ao interruptor q não foi mexido.
+
+Volto para a sala dos interruptores, desligo o interruptor q estava ligado e
+ligo o q não foi mexido na primeira checagem.
+
+2° ida: Ao checar a outra sala, se a lampada estiver desligada e quente, ela é 
+a lampada q está conectada ao interruptor q foi desligado, mas se estiver ligada,
+é a lampada que está conectada ao interruptor q ligamos. Logo concluimos que, na 
+outra sala, a lampada está conectada ao interruptor q sobra.*\
